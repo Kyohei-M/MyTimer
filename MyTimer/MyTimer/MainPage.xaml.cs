6cd37d120005d46d688a9be07380399d4ec0ce48
Kyohei-M <fandltotn@gmail.com>
2018-08-08 02:48:22 +0900

    Add project files.

---
+++ MyTimer/MyTimer/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyTimer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        // 画面が表示されたタイミングでの処理
+        private void MainPageAppearing(object sender, EventArgs e)
+        {
+            // メッセージの購読を設定する
+            // アラートダイアログ表示メッセージを購読する
+            MessagingCenter.Subscribe<MainPageViewModel, AlertParameter>(this, "DisplayAlert", DisplayAlert);
+            // タイマースタートのメッセージを購読する
+            MessagingCenter.Subscribe<MainPageViewModel>(this, "Start", StartTimer);
+        }
+
+        // 画面が表示されなくなったタイミングでの処理
+        private void MainPageDisappearing(object sender, EventArgs e)
+        {
+            // メッセージの購読を解除する
+            MessagingCenter.Unsubscribe<MainPageViewModel, AlertParameter>(this, "DisplayAlert");
+            MessagingCenter.Unsubscribe<MainPageViewModel>(this, "Start");
+        }
+
+        // アラートダイアログを表示する
+        private async void DisplayAlert<T>(T sender, AlertParameter arg)
+        {
+            // アラートダイアログを表示する
+            var isAccept = await DisplayAlert(arg.Title, arg.Message, arg.Accept, arg.Cancel);
+            // アラートダイアログでのユーザーの選択結果い応じた処理を実行する
+            arg.Action?.Invoke(isAccept);
+        }
+
+        // タイマーをスタートする
+        private void StartTimer<T>(T sender)
+        {
+            this.Navigation.PushModalAsync(new CountDownPage());
+        }
+    }
+}
